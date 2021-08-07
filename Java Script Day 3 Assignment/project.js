13ea3e0d6530d38f18f6e373c2075dfe14d27a86
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2021-08-07 23:50:55 +0530

    Java Script Assignment Day3 submitted

---
+++ Java Script Day 3 Assignment/project.js
@@ -0,0 +1,13 @@
+
+function doSomething(){
+
+    let name=document.querySelector("#inp").value;
+    let city=document.querySelector("#inu").value;
+    // console.log(name);
+    //let city=document.querySelector("#inp").value;
+
+   document.querySelector("#sec").innerHTML=`<h1>Name- ${name} ||  City-${city}</h1>`;
+
+}
+
+
